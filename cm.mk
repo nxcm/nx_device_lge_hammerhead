70629c0413135a5dd6c9a7ac2916d8221e512423
AragaoAnderson <andersonaragao@me.com>
2015-03-20 15:01:59 -0300

    Device: Add NX Model name
    Change-Id: Id9fece34894d28e8fca461f6f8d764bd7e32fee7

--- cm.mk
+++ cm.mk
@@ -18,6 +18,7 @@ PRODUCT_NAME := cm_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
+NX_MODEL_NAME := Nexus 5
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
 
